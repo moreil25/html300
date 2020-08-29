fa3322666febac35f0269d899f561d523c350288
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-08-29 13:31:43 -0500

    first commit lesson08

---
+++ lesson07/assignment/my-title/src/components/image-component.vue
@@ -0,0 +1,17 @@
+<script>
+const newComponent = Vue.extend({
+  template: '<div>Custom Images Component</div>'
+})
+  Vue.component('newComponent', {
+    props: ['']
+    data: function() {
+      return {
+        heading: 'Images Component'
+      };
+    }
+  });
+</script>
+
+<template>
+  <callout></callout>
+</template>
