5acd531908285d7f7c25a34a72051660f86e3463
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-08-31 13:31:04 -0500

    added new image to images directory

--- lesson07/assignment/my-title/src/components/image-component.vue
+++ lesson07/assignment/my-title/src/components/image-component.vue
@@ -4,20 +4,43 @@ The image component should have props for at least the image src, alt, & title a
 <script>
 //new Vue({ el: '#custom-wrapper'})
 export default {
-const newComponent = Vue.extend({
-  //custom elements can be over and over, creating new instances in each use
-  template: '<div id="custom-wrapper">Custom Images Component</div>'
-})
-  Vue.component('newComponent', {
+  Vue.component('image-component', {
+
     //custom attributes you can register on a component
     props: ['img', 'src', 'caption', 'alt'],
-    template: '<div>{{ img }}</div>'
-    //data must be a function
+    template: '<div>{{ img. }}</div>'
+    //same fields as instance, but data is no longer an object, but a function that returns object
     data: function() {
       return {
-        heading: 'Images Component'
+        imageArray: [
+        {id: 1,
+        img: 'Cross Stitch',
+        src: 'xstitch.jpg',
+        caption: 'Cross Stitch Flat Lay',
+        alt: 'Flat lay image of cross stitch, Kindle ereader, cup of tea'},
+
+        {id: 2,
+        img: 'Lions',
+        src: 'lions.jpg',
+        caption: 'Crocheted Lions',
+        alt: 'Two small crocheted lions'},
+
+        {id: 3,
+        img: 'Rainbow',
+        src: 'rainbow.jpg',
+        caption: 'Crocheted Rainbow',
+        alt: 'Crocheted rainbow'},
+
+        {id: 4,
+        img: 'Close Up Knit',
+        src: 'knit.jpg',
+        caption: 'Moss Stitch Close Up',
+        alt: 'Close up view of changing yarn colors in knit'}
+      ]
       };
-    }
+    },
+//grab HTML and put it in the template string
+    template: ''
   });
   const borderOn = {
     return {
@@ -33,7 +56,7 @@ const newComponent = Vue.extend({
 </script>
 
 <template>
-  <div id="custom-wrapper">Custom Images Component</div>
+  <imageComponent></imageComponent>
   <!-- v-bind dynamically passes data -->
   <custom-image
     v-bind:id=".id"
@@ -42,29 +65,8 @@ const newComponent = Vue.extend({
 </template>
 
 <!--array of data that you will want to render a component for each one-->
-{id: 1,
-img: 'Cross Stitch',
-src: 'xstitch.jpg',
-caption: 'Cross Stitch Flat Lay',
-alt: 'Flat lay image of cross stitch, Kindle ereader, cup of tea'},
 
-{id: 2,
-img: 'Lions',
-src: 'lions.jpg',
-caption: 'Crocheted Lions',
-alt: 'Two small crocheted lions'},
-
-{id: 3,
-img: 'Rainbow',
-src: 'rainbow.jpg',
-caption: 'Crocheted Rainbow',
-alt: 'Crocheted rainbow'},
-
-{id: 4,
-img: 'Close Up Knit',
-src: 'knit.jpg',
-caption: 'Moss Stitch Close Up',
-alt: 'Close up view of changing yarn colors in knit'}
 
 
 <!-- https://medium.com/@liutingchun_95744/vue-js-making-a-image-component-with-contain-cover-property-7bb196c27a2f -->
+<!--  -->
