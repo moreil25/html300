91f061d327d08222255e37a3ce1c3f2bbbde8d25
moreil@uw.edu <55099479+moreil25@users.noreply.github.com>
2020-09-06 19:09:27 -0500

    troubleshooting errors with filepath clickmixin.js

---
+++ lesson07/assignment/my-title/clickMixin.js
@@ -0,0 +1,18 @@
+// need a mixin declaration
+
+export default {
+    data ()
+    const borderOn = {
+      return {
+        borderOn: true
+      }
+    },
+    methods: {
+      methods: {
+        getBorder(border) {
+          return require(`@/src/${border}`)
+      toggleBorder() {
+        this.borderOn = !this.borderOn;
+      }
+    }
+  }
